cbec9af9e0628e154100eee85f81aa37e0bd7aaf
Milan Krneta <krneta@amazon.com>
2024-06-21 14:47:05 -0700

    comment out index error, for now

--- src/Quasar.jl
+++ src/Quasar.jl
@@ -1443,7 +1443,8 @@ function evaluate_qubits(v::AbstractVisitor, qubit_targets)::Vector{Int}
         elseif head(qubit_expr) == :indexed_identifier
             qubit_name = name(qubit_expr)
             qubit_ix   = evaluate(v, qubit_expr.args[2])
-            haskey(mapping, qubit_name * "[$qubit_ix]") || throw(QasmVisitorError("Invalid qubit index '$qubit_ix' in '$qubit_name'.", "IndexError"))
+            # Need to figure this out
+            # haskey(mapping, qubit_name * "[$qubit_ix]") || throw(QasmVisitorError("Invalid qubit index '$qubit_ix' in '$qubit_name'.", "IndexError"))
             return Iterators.flatten(mapping[qubit_name * "[$rq]"] for rq in qubit_ix)
         elseif head(qubit_expr) == :array_literal
             return evaluate_qubits(v, qubit_expr.args)
